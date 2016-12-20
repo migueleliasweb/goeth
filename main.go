8bba8e5b80a0d8d7859b43aafde1d27e7421e0aa
Miguel E dos Santos <miguel.santos@sieve.com.br>
2016-12-20 13:59:23 -0200

    Almost there

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"strings"
@@ -18,8 +19,8 @@ func getDocker0Network() *net.IPNet {
 func getIps(
 	excludeLocalhost bool,
 	excludeDockerNetwork bool,
-	onlyipv4 bool,
-	onlyipv6 bool) []string {
+	allowpv4 bool,
+	allowipv6 bool) []string {
 
 	adresses, _ := net.InterfaceAddrs()
 	ips := make([]string, 0)
@@ -33,9 +34,9 @@ func getIps(
 			continue
 		} else if excludeDockerNetwork && docker0Network.Contains(parsedIP) {
 			continue
-		} else if onlyipv4 && parsedIP.To4() == nil {
+		} else if allowpv4 && parsedIP.To4() == nil {
 			continue
-		} else if onlyipv6 && parsedIP.To4() != nil {
+		} else if allowipv6 && parsedIP.To4() != nil {
 			continue
 		}
 
@@ -50,20 +51,22 @@ func formatOutput(ips []string, separator string) string {
 }
 
 func main() {
-	excludeLocalhost := true
-	excludeDockerNetwork := true
-	onlyipv4 := true
-	onlyipv6 := false
-	separator := ","
+	excludeLocalhost := flag.Bool("exclude-localhost", false, "Whether to exclude localhost from the result.")
+	excludeDockerNetwork := flag.Bool("exclude-docker-network", false, "Whether to exclude the docker network from the result.")
+	allowipv4 := flag.Bool("allow-ipv4", true, "Whether to allow ipv4 results.")
+	allowipv6 := flag.Bool("allow-ipv6", false, "Whether to allow ipv6 results.")
+	separator := flag.String("separator", ",", "Separator to use on the output.")
+
+	flag.Parse()
 
 	ips := getIps(
-		excludeLocalhost,
-		excludeDockerNetwork,
-		onlyipv4,
-		onlyipv6,
+		*excludeLocalhost,
+		*excludeDockerNetwork,
+		*allowipv4,
+		*allowipv6,
 	)
 
-	output := formatOutput(ips, separator)
+	output := formatOutput(ips, *separator)
 
 	fmt.Println(output)
 }
