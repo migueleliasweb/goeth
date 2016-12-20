84eac074bd157991f86c973f919b7474f1054e18
Miguel E dos Santos <miguel.santos@sieve.com.br>
2016-12-20 14:29:41 -0200

    Working. Need tests.

--- main.go
+++ main.go
@@ -19,7 +19,6 @@ func getDocker0Network() *net.IPNet {
 func getIps(
 	excludeLocalhost bool,
 	excludeDockerNetwork bool,
-	allowpv4 bool,
 	allowipv6 bool) []string {
 
 	adresses, _ := net.InterfaceAddrs()
@@ -34,9 +33,7 @@ func getIps(
 			continue
 		} else if excludeDockerNetwork && docker0Network.Contains(parsedIP) {
 			continue
-		} else if allowpv4 && parsedIP.To4() == nil {
-			continue
-		} else if allowipv6 && parsedIP.To4() != nil {
+		} else if !allowipv6 && parsedIP.To4() == nil {
 			continue
 		}
 
@@ -53,8 +50,7 @@ func formatOutput(ips []string, separator string) string {
 func main() {
 	excludeLocalhost := flag.Bool("exclude-localhost", false, "Whether to exclude localhost from the result.")
 	excludeDockerNetwork := flag.Bool("exclude-docker-network", false, "Whether to exclude the docker network from the result.")
-	allowipv4 := flag.Bool("allow-ipv4", true, "Whether to allow ipv4 results.")
-	allowipv6 := flag.Bool("allow-ipv6", false, "Whether to allow ipv6 results.")
+	allowipv6 := flag.Bool("ipv6", false, "Whether to allow ipv6 results.")
 	separator := flag.String("separator", ",", "Separator to use on the output.")
 
 	flag.Parse()
@@ -62,7 +58,6 @@ func main() {
 	ips := getIps(
 		*excludeLocalhost,
 		*excludeDockerNetwork,
-		*allowipv4,
 		*allowipv6,
 	)
 
