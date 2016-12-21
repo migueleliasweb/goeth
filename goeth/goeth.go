ad8c5fc8ebd91be56d01cd4ce932b74691724a9e
Miguel Elias dos Santos <migueleliasweb@gmail.com>
2016-12-21 01:43:38 -0200

    Tests !

---
+++ goeth/goeth.go
@@ -0,0 +1,120 @@
+package goeth
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"strings"
+)
+
+func getDocker0Network() *net.IPNet {
+	iface, _ := net.InterfaceByName("docker0")
+	addresses, _ := iface.Addrs()
+
+	_, parsedIPNet, _ := net.ParseCIDR(addresses[0].String())
+
+	return parsedIPNet
+}
+
+func isLocalIp(ip net.IP) bool {
+	//https://en.wikipedia.org/wiki/Private_network#Private_IPv4_address_spaces
+	_, classANetwork, _ := net.ParseCIDR("10.0.0.0/8")
+	_, classBNetwork, _ := net.ParseCIDR("172.16.0.0/12")
+	_, classCNetwork, _ := net.ParseCIDR("192.168.0.0/16")
+
+	return classANetwork.Contains(ip) || classBNetwork.Contains(ip) || classCNetwork.Contains(ip) || ip.IsLoopback()
+}
+
+func excludePrivateIPs(ips []string) []string {
+	filteredIPs := make([]string, 0)
+
+	for _, ip := range ips {
+		if isLocalIp(net.ParseIP(ip)) {
+			continue
+		}
+
+		filteredIPs = append(filteredIPs, ip)
+	}
+	return filteredIPs
+}
+
+func excludePublicIPs(ips []string) []string {
+	filteredIPs := make([]string, 0)
+
+	for _, ip := range ips {
+		if !isLocalIp(net.ParseIP(ip)) {
+			continue
+		}
+
+		filteredIPs = append(filteredIPs, ip)
+	}
+	return filteredIPs
+}
+
+func getIps(
+	excludeLocalhost bool,
+	excludeDockerNetwork bool,
+	allowipv6 bool) []string {
+
+	adresses, _ := net.InterfaceAddrs()
+	ips := make([]string, 0)
+
+	docker0Network := getDocker0Network()
+
+	for _, addr := range adresses {
+		parsedIP, _, _ := net.ParseCIDR(addr.String())
+
+		if excludeLocalhost && parsedIP.IsLoopback() {
+			continue
+		} else if excludeDockerNetwork && docker0Network.Contains(parsedIP) {
+			continue
+		} else if !allowipv6 && parsedIP.To4() == nil {
+			continue
+		}
+
+		ips = append(ips, parsedIP.String())
+	}
+
+	return ips
+}
+
+func formatOutput(ips []string, separator string) string {
+	return strings.Join(ips, separator)
+}
+
+func Run(outputBuffer io.Writer) {
+	excludeLocalhost := flag.Bool("exclude-localhost", false, "Whether to exclude localhost from the result.")
+	excludeDockerNetwork := flag.Bool("exclude-docker-network", false, "Whether to exclude the docker network from the result.")
+	allowipv6 := flag.Bool("ipv6", false, "Whether to allow ipv6 results.")
+	separator := flag.String("separator", ",", "Separator to use on the output.")
+
+	onlyPublic := flag.Bool("only-public", false, "Whether to return only public addresses.")
+	onlyPrivate := flag.Bool("only-private", false, "Whether to return only private addresses.")
+
+	flag.Parse()
+
+	if *onlyPrivate && *onlyPublic {
+		fmt.Fprintln(outputBuffer, "Can't use simultaneously 'only-private' and 'only-public' options !")
+		os.Exit(1)
+	}
+
+	ips := getIps(
+		*excludeLocalhost,
+		*excludeDockerNetwork,
+		*allowipv6,
+	)
+
+	if *onlyPrivate {
+		ips = excludePublicIPs(ips)
+	}
+
+	if *onlyPublic {
+		ips = excludePrivateIPs(ips)
+	}
+
+	output := formatOutput(ips, *separator)
+
+	fmt.Fprintln(outputBuffer, output)
+}
---
+++ goeth/goeth_test.go
@@ -0,0 +1,101 @@
+package goeth
+
+import (
+	"net"
+	"testing"
+)
+
+func TestGetDocker0Network(t *testing.T) {
+	resultNetwork := getDocker0Network()
+
+	if !resultNetwork.Contains(resultNetwork.IP) {
+		t.Errorf("Wrong IP configuration for Docker0")
+	}
+}
+
+func TestIsLocalIp(t *testing.T) {
+	privateIPs := []string{
+		"127.0.0.1",
+		"172.17.0.1",
+		"10.168.0.1",
+		"192.168.0.1",
+		getDocker0Network().IP.String(),
+	}
+
+	for _, localIP := range privateIPs {
+		if !isLocalIp(net.ParseIP(localIP)) {
+			t.Errorf("127.0.0.1 must be an local IP address")
+		}
+	}
+}
+
+func TestExcludePrivateIPs(t *testing.T) {
+	IPs := []string{
+		"127.0.0.1",
+		"172.17.0.1",
+		"10.168.0.1",
+		"192.168.0.1",
+		getDocker0Network().IP.String(),
+		"50.50.50.50",
+	}
+
+	filteredIPs := excludePrivateIPs(IPs)
+
+	if len(filteredIPs) != 1 {
+		t.Errorf("Only one IP should be public")
+	}
+}
+
+func TestExcludePublicIPs(t *testing.T) {
+	IPs := []string{
+		"127.0.0.1",
+		"172.17.0.1",
+		"10.168.0.1",
+		"192.168.0.1",
+		getDocker0Network().IP.String(),
+		"50.50.50.50",
+	}
+
+	filteredIPs := excludePublicIPs(IPs)
+
+	if len(filteredIPs) != 5 {
+		t.Errorf("Only one IP should have been filtered")
+	}
+}
+
+func TestGetIPs(t *testing.T) {
+	result := getIps(false, false, true)
+
+	if len(result) == 0 {
+		t.Errorf("Could not find any IP")
+	}
+
+	for _, ip := range result {
+		if net.ParseIP(ip) == nil {
+			t.Errorf("Could not parse IP [%s]", ip)
+		}
+	}
+}
+
+func TestFormatOutput(t *testing.T) {
+	IPs := []string{
+		"127.0.0.1",
+		"50.50.50.50",
+	}
+
+	output := formatOutput(IPs, "|")
+	correct_output := "127.0.0.1|50.50.50.50"
+
+	if output != correct_output {
+		t.Errorf("Wrong output format. Should have been [%s], but was [%s]",
+			correct_output,
+			output,
+		)
+	}
+}
+
+// ????
+// func TestRun(t *testing.T) {
+// 	buffer := bytes.Buffer{}
+// 	Run(buffer)
+// }
